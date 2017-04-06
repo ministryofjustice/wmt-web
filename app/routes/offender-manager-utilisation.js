15e36d7b9c838fd6d9fed4dbe7e7b85f54419938
Steven Cochrane <s.cochrane@kainos.com>
2017-04-06 14:06:05 +0100

    243: removed incorrect POST request, updated GET request to take in date parameter vs year param

--- app/routes/offender-manager-utilisation.js
+++ app/routes/offender-manager-utilisation.js
@@ -2,17 +2,9 @@ const getUtilisationTable = require('../services/get-utilisation-table')
 const ORG_UNIT_TYPE = require('../constants/organisation-unit-type-enum')
 
 module.exports = function (router) {
-  router.get(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:userId/:year/`, function (req, res, next) {
+  router.get(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:id/`, function (req, res, next) {
     return res.render('utilisation', {
-      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.userId, req.params.year)
-    })
-  })
-
-  router.post(`/caseload-utilisation/${ORG_UNIT_TYPE.OFFENDER_MANAGER}/:userId/:year/`, function (req, res, next) {
-    date = req.body.date
-    splitDate = date.split('-', 3)
-    return res.render('utilisation', {
-      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.userId, splitDate[0])
+      utilisation: getUtilisationTable(ORG_UNIT_TYPE.OFFENDER_MANAGER, req.params.id, req.query.date)
     })
   })
 }
