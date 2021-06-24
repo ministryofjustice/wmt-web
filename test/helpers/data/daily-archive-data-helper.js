21aa60a06496465cfa64c17c0e6a94e0ad5e4cfb
carlov20 <carlov20@users.noreply.github.com>
2021-06-24 16:19:51 +0100

    WFP-322: started to change repo for working on cloud platform

---
+++ test/helpers/data/daily-archive-data-helper.js
@@ -0,0 +1,35 @@
+const config = require('../../../knexfile').legacy
+const knex = require('knex')(config)
+
+module.exports.createDailyArchive = function (dailyArchiveData) {
+  const insert = {
+    om_id: 1,
+    om_type_id: 1,
+    workload_id: dailyArchiveData.workloadID,
+    workload_date: dailyArchiveData.workloadDate,
+    ldu_name: dailyArchiveData.lduName,
+    ldu_unique_identifier: '123',
+    region_name: dailyArchiveData.regionName,
+    grade: dailyArchiveData.grade,
+    team_name: dailyArchiveData.teamName,
+    om_name: dailyArchiveData.omName,
+    total_cases: dailyArchiveData.totalCases,
+    total_points: dailyArchiveData.totalPoints,
+    sdr_points: dailyArchiveData.sdrPoints,
+    sdr_conversion_points: dailyArchiveData.sdrConversionPoints,
+    paroms_points: dailyArchiveData.paromsPoints,
+    nominal_target: dailyArchiveData.nominalTarget,
+    contracted_hours: dailyArchiveData.contractedHours,
+    hours_reduction: dailyArchiveData.hoursReduction
+  }
+
+  return knex('daily_archive_data').withSchema('dbo').returning('id').insert(insert)
+    .then(function (ids) {
+      return ids
+    })
+}
+
+module.exports.deleteDailyArchiveByIds = function (idsToDelete) {
+  return knex('daily_archive_data').withSchema('dbo').where('id', idsToDelete)
+    .del()
+}
