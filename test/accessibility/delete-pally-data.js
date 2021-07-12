095413a87242487dbd1dff283c7382762683bb4d
carlov20 <carlov20@users.noreply.github.com>
2021-07-12 08:42:42 +0100

    WFP-325 linting fixes

--- test/accessibility/delete-pally-data.js
+++ test/accessibility/delete-pally-data.js
@@ -3,7 +3,6 @@ const courtReportsDataHelper = require('../helpers/data/court-reports-aggregated
 const path = require('path')
 const { readFileSync } = require('fs')
 
-
 const pallyCourtInserts = path.resolve(__dirname, '../../pallyCourtInserts.json')
 const pallyWorkloadInserts = path.resolve(__dirname, '../../pallyWorkloadInserts.json')
 
@@ -11,8 +10,8 @@ try {
   const courtReportInserts = JSON.parse(readFileSync(pallyCourtInserts, 'utf8'))
   const workloadInserts = JSON.parse(readFileSync(pallyWorkloadInserts, 'utf8'))
 
-  aggregatedDataHelper.removeInsertedData(workloadInserts).then(function() {
-    return courtReportsDataHelper.removeInsertedData(courtReportInserts).then(function() {
+  aggregatedDataHelper.removeInsertedData(workloadInserts).then(function () {
+    return courtReportsDataHelper.removeInsertedData(courtReportInserts).then(function () {
       console.log('all data cleaned up')
       process.exit(0)
     })
--- test/accessibility/setup-pa11y-file.js
+++ test/accessibility/setup-pa11y-file.js
@@ -8,7 +8,7 @@ const pallyCiFile = path.resolve(__dirname, '../../.pa11yci')
 const pallyCourtInserts = path.resolve(__dirname, '../../pallyCourtInserts.json')
 const pallyWorkloadInserts = path.resolve(__dirname, '../../pallyWorkloadInserts.json')
 
-const extractInserts = function(inserts) {
+const extractInserts = function (inserts) {
   return {
     offenderManagerId: inserts.find((item) => item.table === 'workload_owner').id,
     teamId: inserts.find((item) => item.table === 'team').id,
@@ -34,97 +34,81 @@ try {
 
   const urls = [host]
   courtReportsDataHelper.addCourtReportWorkloadsForOffenderManager()
-  .then(function(courtReportInserts) {
-    const extractedCourtReports = extractInserts(courtReportInserts)
-    return aggregatedDataHelper.addWorkloadCapacitiesForOffenderManager().then(function(workloadInserts) {
-      const extractedWorkload = extractInserts(workloadInserts)
+    .then(function (courtReportInserts) {
+      const extractedCourtReports = extractInserts(courtReportInserts)
+      return aggregatedDataHelper.addWorkloadCapacitiesForOffenderManager().then(function (workloadInserts) {
+        const extractedWorkload = extractInserts(workloadInserts)
 
+        const capacityUrl = 'caseload-capacity'
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${capacityUrl}`)
+        urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${capacityUrl}`)
+        urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${capacityUrl}`)
+        urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${capacityUrl}`)
 
-      const offenderManagerId = workloadInserts.find((item) => item.table === 'workload_owner').id
-      const teamId = workloadInserts.find((item) => item.table === 'team').id
-      const lduId = workloadInserts.find((item) => item.table === 'ldu').id
-      const regionId = workloadInserts.find((item) => item.table === 'region').id
-      const courtReporterId = courtReportInserts.find((item) => item.table === 'workload_owner').id
+        const caseProgressUrl = 'case-progress'
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${caseProgressUrl}`)
+        urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${caseProgressUrl}`)
+        urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${caseProgressUrl}`)
+        urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${caseProgressUrl}`)
 
+        const overviewUrl = 'overview'
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${overviewUrl}`)
+        urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${overviewUrl}`)
+        urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${overviewUrl}`)
+        urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${overviewUrl}`)
+        urls.push(`${probationBase}/${national}/${overviewUrl}`)
 
-        
+        urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${overviewUrl}`)
+        urls.push(`${courtReportsBase}/${team}/${extractedCourtReports.teamId}/${overviewUrl}`)
+        urls.push(`${courtReportsBase}/${ldu}/${extractedCourtReports.lduId}/${overviewUrl}`)
+        urls.push(`${courtReportsBase}/${region}/${extractedCourtReports.regionId}/${overviewUrl}`)
+        urls.push(`${courtReportsBase}/${national}/${overviewUrl}`)
 
-      const capacityUrl = 'caseload-capacity'
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${capacityUrl}`)
-      urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${capacityUrl}`)
-      urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${capacityUrl}`)
-      urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${capacityUrl}`)
+        const caseloadUrl = 'caseload'
+        urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${caseloadUrl}`)
+        urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${caseloadUrl}`)
+        urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${caseloadUrl}`)
+        urls.push(`${probationBase}/${national}/${caseloadUrl}`)
 
-      const caseProgressUrl = 'case-progress'
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${caseProgressUrl}`)
-      urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${caseProgressUrl}`)
-      urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${caseProgressUrl}`)
-      urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${caseProgressUrl}`)
+        const contractedHoursUrl = 'contracted-hours'
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${contractedHoursUrl}`)
 
-      const overviewUrl = 'overview'
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${overviewUrl}`)
-      urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${overviewUrl}`)
-      urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${overviewUrl}`)
-      urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${overviewUrl}`)
-      urls.push(`${probationBase}/${national}/${overviewUrl}`)
+        const reductionsUrl = 'reductions'
+        const addReductionsUrl = 'add-reduction'
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${reductionsUrl}`)
+        urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${addReductionsUrl}`)
+        urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${reductionsUrl}`)
+        urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${addReductionsUrl}`)
 
-      urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${overviewUrl}`)
-      urls.push(`${courtReportsBase}/${team}/${extractedCourtReports.teamId}/${overviewUrl}`)
-      urls.push(`${courtReportsBase}/${ldu}/${extractedCourtReports.lduId}/${overviewUrl}`)
-      urls.push(`${courtReportsBase}/${region}/${extractedCourtReports.regionId}/${overviewUrl}`)
-      urls.push(`${courtReportsBase}/${national}/${overviewUrl}`)
+        const admin = 'admin'
+        urls.push(`${host}/${admin}/workload-points`)
+        urls.push(`${host}/${admin}/user`)
+        urls.push(`${host}/${admin}/user-rights`)
+        pa11yJson.urls = urls
 
-      const caseloadUrl = 'caseload'
-      urls.push(`${probationBase}/${team}/${extractedWorkload.teamId}/${caseloadUrl}`)
-      urls.push(`${probationBase}/${ldu}/${extractedWorkload.lduId}/${caseloadUrl}`)
-      urls.push(`${probationBase}/${region}/${extractedWorkload.regionId}/${caseloadUrl}`)
-      urls.push(`${probationBase}/${national}/${caseloadUrl}`)
+        try {
+          fs.writeFileSync(pallyCiFile, JSON.stringify(pa11yJson))
+          fs.writeFileSync(pallyCourtInserts, JSON.stringify(courtReportInserts))
+          fs.writeFileSync(pallyWorkloadInserts, JSON.stringify(workloadInserts))
+        } catch (err) {
+          console.error(err)
+        }
 
-      const contractedHoursUrl = 'contracted-hours'
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${contractedHoursUrl}`)
+        console.log('pa11y ci file updated')
 
-      const reductionsUrl = 'reductions'
-      const addReductionsUrl = 'add-reduction'
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${reductionsUrl}`)
-      urls.push(`${probationBase}/${offenderManager}/${extractedWorkload.offenderManagerId}/${addReductionsUrl}`)
-      urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${reductionsUrl}`)
-      urls.push(`${courtReportsBase}/${offenderManager}/${extractedCourtReports.offenderManagerId}/${addReductionsUrl}`)
+        const req = http.request(`${host}/refresh`, res => {
+          console.log(`statusCode: ${res.statusCode}`)
+          process.exit(0)
+        })
 
-      const admin = 'admin'
-      urls.push(`${host}/${admin}/workload-points`)
-      urls.push(`${host}/${admin}/user`)
-      urls.push(`${host}/${admin}/user-rights`)
-      pa11yJson.urls = urls
+        req.on('error', (err) => {
+          console.error(err)
+          process.exit(1)
+        })
 
-      try {
-        fs.writeFileSync(pallyCiFile, JSON.stringify(pa11yJson))
-        fs.writeFileSync(pallyCourtInserts, JSON.stringify(courtReportInserts))
-        fs.writeFileSync(pallyWorkloadInserts, JSON.stringify(workloadInserts))
-      } catch (err) {
-        console.error(err)
-      }
-
-      try {
-        
-      } catch (err) {
-        console.error(err)
-      }
-      console.log('pa11y ci file updated')
-
-      const req = http.request(`${host}/refresh`, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        process.exit(0)
+        req.end()
       })
-
-      req.on('error', (err) => {
-        console.error(err)
-        process.exit(1)
-      })
-
-      req.end()
     })
-  })
 } catch (err) {
   console.error(err)
 }
-
