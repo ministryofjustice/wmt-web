be1a70c7a0f8980320a23c62d34d17ac4c58551e
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-07-26 21:03:25 +0100

    WFP-320 reorganise test

--- test/e2e/view-caseload-overview.js
+++ test/e2e/view-caseload-overview.js
@@ -10,7 +10,7 @@ let workloadOwnerDefaultUrl
 let teamDefaultUrl
 let lduDefaultUrl
 let regionDefaultUrl
-let nationalDefaultUrl
+const nationalDefaultUrl = '/' + workloadTypes.PROBATION + '/hmpps/0'
 
 describe('View overview', function () {
   before(async function () {
@@ -22,108 +22,112 @@ describe('View overview', function () {
     teamDefaultUrl = '/' + workloadTypes.PROBATION + '/team/' + workloadOwnerIds.filter((item) => item.table === 'team')[0].id
     lduDefaultUrl = '/' + workloadTypes.PROBATION + '/ldu/' + workloadOwnerIds.filter((item) => item.table === 'ldu')[0].id
     regionDefaultUrl = '/' + workloadTypes.PROBATION + '/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id
-    nationalDefaultUrl = '/' + workloadTypes.PROBATION + '/hmpps/0'
     workloadOwnerGrade = await dataHelper.selectGradeForWorkloadOwner(workloadOwnerId)
   })
 
-  it('should navigate to the workload owner overview page', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    let grade = await $('.sln-grade')
-    grade = await grade.getText()
-    expect(grade).to.equal(workloadOwnerGrade)
+  describe('workload owner level', function () {
+    beforeEach(async function () {
+      await browser.url(workloadOwnerDefaultUrl + '/overview')
+    })
+
+    it('should navigate to the workload owner overview page', async function () {
+      let grade = await $('.sln-grade')
+      grade = await grade.getText()
+      expect(grade).to.equal(workloadOwnerGrade)
+    })
+
+    it('should not include the reductions export for staff at workload owner level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
   })
 
-  it('should not include the reductions export at workload owner level', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
+  describe('team level', function () {
+    beforeEach(async function () {
+      await browser.url(workloadOwnerDefaultUrl + '/overview')
+      const teamLink = await $('[href="' + teamDefaultUrl + '"]')
+      await teamLink.click()
+      const teamOverviewLink = await $('[href="' + teamDefaultUrl + '/overview"]')
+      await teamOverviewLink.click()
+    })
+
+    it('should navigate to the team overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Offender Manager')
+    })
+
+    it('should not include the reductions export for staff at team level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
   })
 
-  it('should navigate to the team overview page', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const teamLink = await $('[href="' + teamDefaultUrl + '"]')
-    await teamLink.click()
-    const teamOverviewLink = await $('[href="' + teamDefaultUrl + '/overview"]')
-    await teamOverviewLink.click()
-    const element = await $('.sln-table-org-level')
-    const text = await element.getText()
-    expect(text).to.equal('Offender Manager')
+  describe('ldu level', function () {
+    beforeEach(async function () {
+      await browser.url(workloadOwnerDefaultUrl + '/overview')
+      const lduLink = await $('[href="' + lduDefaultUrl + '"]')
+      await lduLink.click()
+      const lduOverviewLink = await $('[href="' + lduDefaultUrl + '/overview"]')
+      await lduOverviewLink.click()
+    })
+
+    it('should navigate to the ldu overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Team')
+    })
+
+    it('should not include the reductions export for staff at ldu level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
   })
 
-  it('should navigate to the ldu overview page', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const lduLink = await $('[href="' + lduDefaultUrl + '"]')
-    await lduLink.click()
-    const lduOverviewLink = await $('[href="' + lduDefaultUrl + '/overview"]')
-    await lduOverviewLink.click()
-    const element = await $('.sln-table-org-level')
-    const text = await element.getText()
-    expect(text).to.equal('Team')
+  describe('region level', function () {
+    beforeEach(async function () {
+      await browser.url(workloadOwnerDefaultUrl + '/overview')
+      const regionLink = await $('[href="' + regionDefaultUrl + '"]')
+      await regionLink.click()
+      const regionOverviewLink = await $('[href="' + regionDefaultUrl + '/overview"]')
+      await regionOverviewLink.click()
+    })
+
+    it('should navigate to the region overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Probation Delivery Unit')
+    })
+
+    it('should not include the reductions export for staff at region level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
   })
 
-  it('should navigate to the region overview page', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const regionLink = await $('[href="' + regionDefaultUrl + '"]')
-    await regionLink.click()
-    const regionOverviewLink = await $('[href="' + regionDefaultUrl + '/overview"]')
-    await regionOverviewLink.click()
-    const element = await $('.sln-table-org-level')
-    const text = await element.getText()
-    expect(text).to.equal('Probation Delivery Unit')
-  })
+  describe('national level', function () {
+    beforeEach(async function () {
+      await browser.url(nationalDefaultUrl)
+      const overviewLink = await $('[href="' + nationalDefaultUrl + '/overview"]')
+      await overviewLink.click()
+    })
 
-  it('should navigate to the national overview page', async function () {
-    await browser.url(nationalDefaultUrl)
-    const overviewLink = await $('[href="' + nationalDefaultUrl + '/overview"]')
-    await overviewLink.click()
-    const element = await $('.sln-table-org-level')
-    const text = await element.getText()
-    expect(text).to.equal('Region')
-  })
+    it('should navigate to the national overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Region')
+    })
 
-  it('should not include the reductions export for staff at team level', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const teamLink = await $('[href="' + teamDefaultUrl + '"]')
-    await teamLink.click()
-    const teamOverviewLink = await $('[href="' + teamDefaultUrl + '/overview"]')
-    await teamOverviewLink.click()
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
-  })
-
-  it('should not include the reductions export for staff at region level', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    const regionLink = await $('[href="' + regionDefaultUrl + '"]')
-    await regionLink.click()
-    const regionOverviewLink = await $('[href="' + regionDefaultUrl + '/overview"]')
-    await regionOverviewLink.click()
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
-  })
-
-  it('should not include the reductions export for staff at ldu level', async function () {
-    await browser.url(lduDefaultUrl + '/overview')
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
-  })
-
-  it('should not include the reductions export for staff at national level', async function () {
-    await browser.url(nationalDefaultUrl + '/overview')
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
-  })
-
-  it('should not include the reductions export for staff at workload owner level', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-
-    const reductionExport = await $('.reduction-export')
-    const exists = await reductionExport.isExisting()
-    return expect(exists).to.be.false
+    it('should not include the reductions export for staff at national level', async function () {
+      await browser.url(nationalDefaultUrl + '/overview')
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
   })
 
   it('should allow the user to navigate down the org hierarchy from the national page', async function () {
@@ -214,6 +218,16 @@ describe('View overview', function () {
     pageTitle = await $('.govuk-caption-xl')
     text = await pageTitle.getText()
     expect(text).to.equal('National')
+  })
+
+  after(function () {
+    authenticationHelper.logout()
+  })
+})
+
+describe('overview for managers', function () {
+  before(async function () {
+    await authenticationHelper.login(authenticationHelper.users.Manager)
   })
 
   it('should not include the reductions export for managers at workload owner level', async function () {
