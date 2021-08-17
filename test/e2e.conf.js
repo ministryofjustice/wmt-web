05467a08242fc9b864bbcf277d878770f4f9e464
carlov20 <carlov20@users.noreply.github.com>
2021-08-17 16:46:10 +0100

    WFP-474 hardcoded chrome version

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -4,7 +4,7 @@ let reportAggregator
 exports.config = {
   services: [
     [
-      'selenium-standalone',
+      ['selenium-standalone', { drivers: { chrome: '91.0.4472.101' } }],
       {
         logs: 'logs'
       }
