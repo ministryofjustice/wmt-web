2282dd884bc8f5b5a44f94f49f1ec8e0fe539cfa
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-08-17 10:47:01 +0100

    WFP-477 simplified org-unit-finder

--- app/services/helpers/org-unit-finder.js
+++ app/services/helpers/org-unit-finder.js
@@ -1,9 +1,7 @@
 const orgUnit = require('../../constants/organisation-unit')
 
 module.exports = function (property, value) {
-  const newObj = Object.entries(orgUnit).find(function ([, v]) {
+  return Object.values(orgUnit).find(function (v) {
     return v[property] === value
   })
-
-  return newObj ? newObj[1] : undefined
 }
