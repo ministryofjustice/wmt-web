4813b9b559f3b5c348ec73e05f3c85edc8f9f23f
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-11-05 10:55:30 +0000

    WFP-351 remove everything except self from defaultSrc

--- app/middleware/setUpWebSecurity.js
+++ app/middleware/setUpWebSecurity.js
@@ -14,7 +14,7 @@ module.exports = function () {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          defaultSrc: ["'self'", 'https://www.google-analytics.com', 'www.google-analytics.com'],
+          defaultSrc: ["'self'"],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
           scriptSrc: ["'self'",
             "'unsafe-eval'",
@@ -24,8 +24,8 @@ module.exports = function () {
             (req, res) => `'nonce-${res.locals.cspNonce}'`,
             'https://www.googletagmanager.com'],
           styleSrc: ["'self'", 'code.jquery.com', "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='", "'sha256-7ynffF12wDROtneyCyZQeDPLI7U5bQGlCJO3PFWnaTg='"],
-          imgSrc: ["'self'"],
-          connectSrc: ["'self'", 'www.googletagmanager.com'],
+          imgSrc: ["'self'", 'https://www.google-analytics.com'],
+          connectSrc: ["'self'", 'www.googletagmanager.com', 'https://www.google-analytics.com'],
           fontSrc: ["'self'"]
         }
       }
