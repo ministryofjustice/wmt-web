09c7d07b7cb10d4d56b221b533f0adf76c388532
carlov20 <carlov20@users.noreply.github.com>
2021-11-18 16:47:31 +0000

    WFP-678 wrote contracted hours test

--- test/e2e/view-contracted-hours.js
+++ test/e2e/view-contracted-hours.js
@@ -8,76 +8,179 @@ let workloadOwnerId
 let workloadOwnerDefaultUrl
 
 describe('View contracted hours', function () {
-  before(async function () {
-    await authenticationHelp.login(authenticationHelp.users.Manager)
-    const results = await dataHelper.selectIdsForWorkloadOwner()
-    workloadOwnerIds = results
-    workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
-    workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
-    await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+  describe('Manager', function () {
+    before(async function () {
+      await authenticationHelp.login(authenticationHelp.users.Manager)
+      const results = await dataHelper.selectIdsForWorkloadOwner()
+      workloadOwnerIds = results
+      workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
+      workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
+      await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+    })
+
+    it('should navigate to the workload owner contracted-hours page', async function () {
+      const breadcrumbs = await $('.govuk-breadcrumbs')
+      let exists = await breadcrumbs.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const subnav = await $('.wmt-sub-nav')
+      exists = await subnav.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const actionForm = await $('.sln-form-action')
+      exists = await actionForm.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const pageTitle = await $('.govuk-heading-l')
+      const text = await pageTitle.getText()
+      expect(text).to.equal('Contracted hours')
+    })
+
+    it('should be able to edit contracted hours', async function () {
+      const contractedHoursField = await $('#contracted-hours')
+      const submit = await $('.govuk-button')
+      await contractedHoursField.setValue('5')
+      await submit.click()
+
+      const successBanner = await $('.govuk-notification-banner--success .govuk-notification-banner__heading')
+      const successBannerText = await successBanner.getText()
+      expect(successBannerText).to.equal('You have successfully updated the contracted hours for Test_Forename Test_Surname')
+    })
+
+    after(function () {
+      authenticationHelp.logout()
+    })
   })
 
-  it('should navigate to the workload owner contracted-hours page', async function () {
-    await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+  describe('Super User', function () {
+    before(async function () {
+      await authenticationHelp.login(authenticationHelp.users.SuperUser)
+      const results = await dataHelper.selectIdsForWorkloadOwner()
+      workloadOwnerIds = results
+      workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
+      workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
+      await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+    })
 
-    const breadcrumbs = await $('.govuk-breadcrumbs')
-    let exists = await breadcrumbs.isExisting()
-    expect(exists).to.be.equal(true)
+    it('should navigate to the workload owner contracted-hours page', async function () {
+      const breadcrumbs = await $('.govuk-breadcrumbs')
+      let exists = await breadcrumbs.isExisting()
+      expect(exists).to.be.equal(true)
 
-    const subnav = await $('.wmt-sub-nav')
-    exists = await subnav.isExisting()
-    expect(exists).to.be.equal(true)
+      const subnav = await $('.wmt-sub-nav')
+      exists = await subnav.isExisting()
+      expect(exists).to.be.equal(true)
 
-    const actionForm = await $('.sln-form-action')
-    exists = await actionForm.isExisting()
-    expect(exists).to.be.equal(true)
+      const actionForm = await $('.sln-form-action')
+      exists = await actionForm.isExisting()
+      expect(exists).to.be.equal(true)
 
-    const link = await $('[href="' + workloadOwnerDefaultUrl + '/overview"]')
-    await link.click()
+      const pageTitle = await $('.govuk-heading-l')
+      const text = await pageTitle.getText()
+      expect(text).to.equal('Contracted hours')
+    })
 
-    const pageTitle = await $('.govuk-caption-xl')
-    const text = await pageTitle.getText()
-    expect(text).to.equal('Offender Manager')
+    it('should be able to edit contracted hours', async function () {
+      const contractedHoursField = await $('#contracted-hours')
+      const submit = await $('.govuk-button')
+      await contractedHoursField.setValue('5')
+      await submit.click()
+
+      const successBanner = await $('.govuk-notification-banner--success .govuk-notification-banner__heading')
+      const successBannerText = await successBanner.getText()
+      expect(successBannerText).to.equal('You have successfully updated the contracted hours for Test_Forename Test_Surname')
+    })
+
+    after(function () {
+      authenticationHelp.logout()
+    })
   })
 
-  it('should be accessible via the Contracted Hours tab when on any other tab', async () => {
-    await browser.url(workloadOwnerDefaultUrl + '/overview')
-    let subnav = await $('.wmt-sub-nav')
-    let exists = await subnav.isExisting()
-    expect(exists).to.be.equal(true)
+  describe('Staff', function () {
+    before(async function () {
+      await authenticationHelp.login(authenticationHelp.users.Staff)
+      const results = await dataHelper.selectIdsForWorkloadOwner()
+      workloadOwnerIds = results
+      workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
+      workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
+      await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+    })
 
-    let contractedHoursLink = await $('[href="' + workloadOwnerDefaultUrl + '/contracted-hours"]')
-    await contractedHoursLink.click()
-    let actionForm = await $('.sln-form-action')
-    exists = await actionForm.isExisting()
-    expect(exists).to.be.equal(true)
+    it('should navigate to the workload owner contracted-hours page', async function () {
+      const breadcrumbs = await $('.govuk-breadcrumbs')
+      let exists = await breadcrumbs.isExisting()
+      expect(exists).to.be.equal(true)
 
-    const caseloadCapacityLink = await $('[href="' + workloadOwnerDefaultUrl + '/caseload-capacity"]')
-    await caseloadCapacityLink.click()
-    subnav = await $('.wmt-sub-nav')
-    exists = await subnav.isExisting()
-    expect(exists).to.be.equal(true)
+      const subnav = await $('.wmt-sub-nav')
+      exists = await subnav.isExisting()
+      expect(exists).to.be.equal(true)
 
-    contractedHoursLink = await $('[href="' + workloadOwnerDefaultUrl + '/contracted-hours"]')
-    await contractedHoursLink.click()
-    actionForm = await $('.sln-form-action')
-    exists = await actionForm.isExisting()
-    expect(exists).to.be.equal(true)
+      const actionForm = await $('.sln-form-action')
+      exists = await actionForm.isExisting()
+      expect(exists).to.be.equal(true)
 
-    const caseProgressLink = await $('[href="' + workloadOwnerDefaultUrl + '/case-progress"]')
-    await caseProgressLink.click()
-    subnav = await $('.wmt-sub-nav')
-    exists = await subnav.isExisting()
-    expect(exists).to.be.equal(true)
+      const pageTitle = await $('.govuk-heading-l')
+      const text = await pageTitle.getText()
+      expect(text).to.equal('Contracted hours')
+    })
 
-    contractedHoursLink = await $('[href="' + workloadOwnerDefaultUrl + '/contracted-hours"]')
-    await contractedHoursLink.click()
-    actionForm = await $('.sln-form-action')
-    exists = await actionForm.isExisting()
-    expect(exists).to.be.equal(true)
+    it('should not be able to edit contracted hours', async function () {
+      const contractedHoursField = await $('#contracted-hours')
+      const submit = await $('.govuk-button')
+      await contractedHoursField.setValue('5')
+      await submit.click()
+
+      const header = await $('.govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
+    })
+
+    after(function () {
+      authenticationHelp.logout()
+    })
   })
 
-  after(function () {
-    authenticationHelp.logout()
+  describe('Application Support', function () {
+    before(async function () {
+      await authenticationHelp.login(authenticationHelp.users.ApplicationSupport)
+      const results = await dataHelper.selectIdsForWorkloadOwner()
+      workloadOwnerIds = results
+      workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
+      workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
+      await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+    })
+
+    it('should navigate to the workload owner contracted-hours page', async function () {
+      const breadcrumbs = await $('.govuk-breadcrumbs')
+      let exists = await breadcrumbs.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const subnav = await $('.wmt-sub-nav')
+      exists = await subnav.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const actionForm = await $('.sln-form-action')
+      exists = await actionForm.isExisting()
+      expect(exists).to.be.equal(true)
+
+      const pageTitle = await $('.govuk-heading-l')
+      const text = await pageTitle.getText()
+      expect(text).to.equal('Contracted hours')
+    })
+
+    it('should not be able to edit contracted hours', async function () {
+      const contractedHoursField = await $('#contracted-hours')
+      const submit = await $('.govuk-button')
+      await contractedHoursField.setValue('5')
+      await submit.click()
+
+      const header = await $('.govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
+    })
+
+    after(function () {
+      authenticationHelp.logout()
+    })
   })
 })
