c0857f61ca858758fe5d4a721c704366311f090f
carlov20 <carlov20@users.noreply.github.com>
2021-11-18 11:31:08 +0000

    WFP-678 wrote some more tests

--- test/e2e/view-caseload-overview-region.js
+++ test/e2e/view-caseload-overview-region.js
@@ -4,8 +4,6 @@ const dataHelper = require('../helpers/data/aggregated-data-helper')
 const workloadTypes = require('../../app/constants/workload-type')
 
 let workloadOwnerIds = []
-let workloadOwnerId
-let workloadOwnerDefaultUrl
 
 let regionDefaultUrl
 
@@ -15,42 +13,43 @@ describe('Region', function () {
       await authenticationHelper.login(authenticationHelper.users.Staff)
       const results = await dataHelper.selectIdsForWorkloadOwner()
       workloadOwnerIds = results
-      workloadOwnerId = workloadOwnerIds.filter((item) => item.table === 'workload_owner')[0].id
-      workloadOwnerDefaultUrl = '/' + workloadTypes.PROBATION + '/offender-manager/' + workloadOwnerId
       regionDefaultUrl = '/' + workloadTypes.PROBATION + '/region/' + workloadOwnerIds.filter((item) => item.table === 'region')[0].id
     })
 
-    describe('region level', function () {
-      beforeEach(async function () {
-        await browser.url(workloadOwnerDefaultUrl + '/overview')
-        const regionLink = await $('[href="' + regionDefaultUrl + '"]')
-        await regionLink.click()
-      })
+    beforeEach(async function () {
+      await browser.url(regionDefaultUrl + '/overview')
+    })
 
-      it('should navigate to the region overview page', async function () {
-        const element = await $('.sln-table-org-level')
-        const text = await element.getText()
-        expect(text).to.equal('Probation Delivery Unit')
-      })
+    it('should navigate to the region overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Probation Delivery Unit')
+    })
 
-      it('should not include the reductions export at region level', async function () {
-        const reductionExport = await $('.reduction-export')
-        const exists = await reductionExport.isExisting()
-        return expect(exists).to.be.false
-      })
+    it('should not include the reductions export at region level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.false
+    })
 
-      it('should not include the overview export at national level', async function () {
-        const exportButton = await $('.sln-export')
-        const exists = await exportButton.isExisting()
-        return expect(exists).to.be.false
-      })
+    it('should not include the overview export at region level', async function () {
+      const exportButton = await $('.sln-export')
+      const exists = await exportButton.isExisting()
+      return expect(exists).to.be.false
+    })
 
-      it('should not be able to download overview', async function () {
-        await browser.url(regionDefaultUrl + '/overview/caseload-csv')
-        const header = await $('govuk-heading-xl')
-        const text = await header.getText()
-        expect(text).to.equal('Access is denied')
-      })
+    it('should not be able to download overview', async function () {
+      await browser.url(regionDefaultUrl + '/overview/caseload-csv')
+      const header = await $('govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
+    })
+
+    it('should not be able to download reductions', async function () {
+      await browser.url(regionDefaultUrl + '/overview/reductions-csv')
+      const header = await $('govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
     })
 
     after(function () {
@@ -61,6 +60,25 @@ describe('Region', function () {
   describe('overview for managers', function () {
     before(async function () {
       await authenticationHelper.login(authenticationHelper.users.Manager)
+      await browser.url(regionDefaultUrl + '/overview')
+    })
+
+    it('should navigate to the region overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Probation Delivery Unit')
+    })
+
+    it('should include the reductions export at region level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.true
+    })
+
+    it('should include the overview export at region level', async function () {
+      const exportButton = await $('.sln-export')
+      const exists = await exportButton.isExisting()
+      return expect(exists).to.be.true
     })
 
     after(function () {
@@ -71,25 +89,27 @@ describe('Region', function () {
   describe('overview for Application Support', function () {
     before(async function () {
       await authenticationHelper.login(authenticationHelper.users.ApplicationSupport)
+      await browser.url(regionDefaultUrl + '/overview')
     })
 
-    describe('regional level', function () {
-      before(async function () {
-        await browser.url(regionDefaultUrl + '/overview')
-      })
+    it('should not include the overview export', async function () {
+      const exportButton = await $('.sln-export')
+      const exists = await exportButton.isExisting()
+      return expect(exists).to.be.false
+    })
 
-      it('should not include the overview export', async function () {
-        const exportButton = await $('.sln-export')
-        const exists = await exportButton.isExisting()
-        return expect(exists).to.be.false
-      })
+    it('should not be able to download overview', async function () {
+      await browser.url(regionDefaultUrl + '/overview/caseload-csv')
+      const header = await $('govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
+    })
 
-      it('should not be able to download overview', async function () {
-        await browser.url(regionDefaultUrl + '/overview/caseload-csv')
-        const header = await $('govuk-heading-xl')
-        const text = await header.getText()
-        expect(text).to.equal('Access is denied')
-      })
+    it('should not be able to download reductions', async function () {
+      await browser.url(regionDefaultUrl + '/overview/reductions-csv')
+      const header = await $('govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
     })
 
     after(function () {
@@ -100,6 +120,25 @@ describe('Region', function () {
   describe('overview for Super User', function () {
     before(async function () {
       await authenticationHelper.login(authenticationHelper.users.SuperUser)
+      await browser.url(regionDefaultUrl + '/overview')
+    })
+
+    it('should navigate to the region overview page', async function () {
+      const element = await $('.sln-table-org-level')
+      const text = await element.getText()
+      expect(text).to.equal('Probation Delivery Unit')
+    })
+
+    it('should include the reductions export at region level', async function () {
+      const reductionExport = await $('.reduction-export')
+      const exists = await reductionExport.isExisting()
+      return expect(exists).to.be.true
+    })
+
+    it('should include the overview export at region level', async function () {
+      const exportButton = await $('.sln-export')
+      const exists = await exportButton.isExisting()
+      return expect(exists).to.be.true
     })
 
     after(function () {
