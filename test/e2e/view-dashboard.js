be49c5200a024f3e2e4f6cfb69141683b7c3bb5f
carlov20 <carlov20@users.noreply.github.com>
2021-11-19 14:02:44 +0000

    WFP-678 wrote dashboard tests

--- test/e2e/view-dashboard.js
+++ test/e2e/view-dashboard.js
@@ -7,25 +7,120 @@ describe('view dashboard reports', function () {
     return fileUrlElement.getAttribute('href')
   }
 
-  before(async function () {
-    await authenticationHelper.login(authenticationHelper.users.SuperUser)
-    const link = await $('[href="/probation/hmpps/0/dashboard"]')
-    await link.click()
+  describe('Staff', function () {
+    before(async function () {
+      await authenticationHelper.login(authenticationHelper.users.Staff)
+      await browser.url('/')
+    })
+
+    it('Dashboard tab does not exist on the home page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      const linkExists = await link.isExisting()
+      expect(linkExists).to.be.equal(false)
+    })
+
+    it('should not be able to go to dashboard page', async function () {
+      await browser.url('/probation/hmpps/0/dashboard')
+      const header = await $('.govuk-heading-xl')
+      const text = await header.getText()
+      expect(text).to.equal('Access is denied')
+    })
+
+    after(async function () {
+      await authenticationHelper.logout()
+    })
   })
 
-  it('shows five dashboard links in date order', async function () {
-    const rows = await browser.findElements('xpath', '//*[@id="dashboard-table"]/tbody/tr')
+  describe('Manager', function () {
+    before(async function () {
+      await authenticationHelper.login(authenticationHelper.users.Manager)
+      await browser.url('/')
+    })
 
-    expect(rows.length).to.equal(5)
+    it('Dashboard tab exists on the home page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      const linkExists = await link.isExisting()
+      expect(linkExists).to.be.equal(true)
+    })
 
-    const firstFileUrl = await extractFileUrl(await $(rows[0]))
-    expect(firstFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210802062147.txt')
+    it('should be able to navigate to dashboard page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      await link.click()
+      const rows = await browser.findElements('xpath', '//*[@id="dashboard-table"]/tbody/tr')
 
-    const lastFileUrl = await extractFileUrl(await $(rows[4]))
-    expect(lastFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210729062147.txt')
+      expect(rows.length).to.equal(5)
+
+      const firstFileUrl = await extractFileUrl(await $(rows[0]))
+      expect(firstFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210802062147.txt')
+
+      const lastFileUrl = await extractFileUrl(await $(rows[4]))
+      expect(lastFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210729062147.txt')
+    })
+
+    after(async function () {
+      await authenticationHelper.logout()
+    })
   })
 
-  after(async function () {
-    await authenticationHelper.logout()
+  describe('Application Support', function () {
+    before(async function () {
+      await authenticationHelper.login(authenticationHelper.users.ApplicationSupport)
+      await browser.url('/')
+    })
+
+    it('Dashboard tab exists on the home page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      const linkExists = await link.isExisting()
+      expect(linkExists).to.be.equal(true)
+    })
+
+    it('should be able to navigate to dashboard page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      await link.click()
+      const rows = await browser.findElements('xpath', '//*[@id="dashboard-table"]/tbody/tr')
+
+      expect(rows.length).to.equal(5)
+
+      const firstFileUrl = await extractFileUrl(await $(rows[0]))
+      expect(firstFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210802062147.txt')
+
+      const lastFileUrl = await extractFileUrl(await $(rows[4]))
+      expect(lastFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210729062147.txt')
+    })
+
+    after(async function () {
+      await authenticationHelper.logout()
+    })
+  })
+
+  describe('Super User', function () {
+    before(async function () {
+      await authenticationHelper.login(authenticationHelper.users.SuperUser)
+      await browser.url('/')
+    })
+
+    it('Dashboard tab exists on the home page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      const linkExists = await link.isExisting()
+      expect(linkExists).to.be.equal(true)
+    })
+
+    it('should be able to navigate to dashboard page', async function () {
+      const link = await $('[href="/probation/hmpps/0/dashboard"]')
+      await link.click()
+      const rows = await browser.findElements('xpath', '//*[@id="dashboard-table"]/tbody/tr')
+
+      expect(rows.length).to.equal(5)
+
+      const firstFileUrl = await extractFileUrl(await $(rows[0]))
+      expect(firstFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210802062147.txt')
+
+      const lastFileUrl = await extractFileUrl(await $(rows[4]))
+      expect(lastFileUrl).to.equal('/probation/hmpps/0/dashboard/download?id=generated-dashboards/dashboard_20210729062147.txt')
+    })
+
+    after(async function () {
+      await authenticationHelper.logout()
+    })
   })
 })
