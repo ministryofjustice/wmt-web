9b1b198dc81c925f08b1e0b8735b5b914e5a235c
carlov20 <carlov20@users.noreply.github.com>
2021-11-23 11:12:36 +0000

    WFP-758 standard fix

--- app/middleware/setupCacheControl.js
+++ app/middleware/setupCacheControl.js
@@ -1,8 +1,8 @@
 
-module.exports = function() {
-    return function(req,res,next) {
-        res.set('Cache-control', 'no-store')
-        res.set('Pragma', 'no-cache')
-        next()
-    }
+module.exports = function () {
+  return function (req, res, next) {
+    res.set('Cache-control', 'no-store')
+    res.set('Pragma', 'no-cache')
+    next()
+  }
 }
