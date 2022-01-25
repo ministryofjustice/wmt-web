43bdadd311bd0e91f65dc06cbb59363dc8924f54
carlov20 <carlov20@users.noreply.github.com>
2022-01-25 11:26:48 +0000

    WFP-858 enabled all e2e tests

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -10,7 +10,7 @@ exports.config = {
       }
     ]
   ],
-  specs: ['./test/e2e/view-overview-om.js'],
+  specs: ['./test/e2e/**/*.js'],
   exclude: [],
   maxInstances: 1,
   baseUrl: process.env.WMT_BASE_URL || 'http://localhost:3000',
