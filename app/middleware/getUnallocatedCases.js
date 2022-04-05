70e07edc50873904eb307172e0dcdcce132a83d4
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2022-04-05 10:58:14 +0100

    :recycle: WFP-991: only get allocations count when user has allocations role

--- app/middleware/getUnallocatedCases.js
+++ app/middleware/getUnallocatedCases.js
@@ -4,7 +4,9 @@ const allocationsClient = require('../data/allocationsClient')
 module.exports = function () {
   return async function (req, res, next) {
     try {
-      res.locals.allocations = await allocationsClient.getAllocations(res.locals.user.token)
+      if (res.locals.canAllocate) {
+        res.locals.allocations = await allocationsClient.getAllocations(res.locals.user.token)
+      }
       next()
     } catch (error) {
       logger.error(error, 'Failed to retrieve unallocated cases')
