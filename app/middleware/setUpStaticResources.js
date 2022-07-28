930bdb370bf714257d3441bb7a287880fbbd7db6
carlov20 <carlov20@users.noreply.github.com>
2022-07-28 11:47:06 +0100

    WFP-1216 added favicon route

--- app/middleware/setUpStaticResources.js
+++ app/middleware/setUpStaticResources.js
@@ -1,12 +1,19 @@
 const path = require('path')
 const compression = require('compression')
 const express = require('express')
+const config = require('../../config')
 
 module.exports = function () {
   const router = express.Router()
 
   router.use(compression())
-  router.use('/public', express.static(path.join(process.cwd(), '/app/public')))
+
+  //  Static Resources Configuration
+  const cacheControl = { maxAge: config.staticResourceCacheDuration * 1000 }
+
+  router.use('/public', express.static(path.join(process.cwd(), '/app/public'), cacheControl))
+
+  router.use('/favicon.ico', express.static(path.join(process.cwd(), '/app/public/images/favicon.ico'), cacheControl))
 
   return router
 }
