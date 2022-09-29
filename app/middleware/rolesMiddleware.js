9b2da76c8dd4ae36ef2bafef2887bee6ad91d686
carlov20 <carlov20@users.noreply.github.com>
2022-09-29 13:30:30 +0100

    WFP-1261 removed expiring reductions (#739)

--- app/middleware/rolesMiddleware.js
+++ app/middleware/rolesMiddleware.js
@@ -3,7 +3,6 @@ module.exports = function () {
   return async function (req, res, next) {
     if (res.locals.user) {
       res.locals.canViewAdmin = [userRoles.SUPER_USER, userRoles.APPLICATION_SUPPORT].includes(res.locals.user.user_role)
-      res.locals.canViewExpiringReductions = [userRoles.SUPER_USER, userRoles.APPLICATION_SUPPORT, userRoles.MANAGER].includes(res.locals.user.user_role)
       res.locals.canExportReductions = [userRoles.SUPER_USER, userRoles.MANAGER].includes(res.locals.user.user_role)
       res.locals.canExportOverview = [userRoles.SUPER_USER, userRoles.MANAGER, userRoles.STAFF].includes(res.locals.user.user_role)
     }
--- app/services/user-search-service.js
+++
@@ -1,7 +0,0 @@
-const searchForUser = require('./data/search-for-user')
-
-module.exports = function () {
-  return searchForUser().then(function (users) {
-    return users
-  })
-}
