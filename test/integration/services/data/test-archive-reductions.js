00bfccf7e22ec618c5d7d305a0d6926da09435a1
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-11-08 10:21:42 +0000

    WFP-1208: specify the person we are searching for

--- test/integration/services/data/test-archive-reductions.js
+++ test/integration/services/data/test-archive-reductions.js
@@ -24,7 +24,7 @@ const archiveDataForm = new ArchiveDataForm(
   toDate.date(),
   toDate.month() + 1,
   toDate.year(),
-  ['data'],
+  ['Reduction Submitter 5'],
   true
 )
 describe('services/data/get-archived-reductions', function () {
