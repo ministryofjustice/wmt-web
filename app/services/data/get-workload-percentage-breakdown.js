24aa11781b4befb2fc0427923e5b4c67d58c4d56
carlov20 <carlov20@users.noreply.github.com>
2023-01-11 14:53:43 +0000

    WFP-1746 removed columns (#904)

--- app/services/data/get-workload-percentage-breakdown.js
+++ app/services/data/get-workload-percentage-breakdown.js
@@ -9,11 +9,7 @@ module.exports = function (id, organisationLevel) {
     'om_name AS omName',
     'grade_code AS omGrade',
     'total_case_points AS totalCasePoints',
-    'arms_community_points AS armsCommunityPoints',
-    'arms_licence_points AS armsLicencePoints',
     'cms_adjustment_points AS cmsPoints',
-    'gs_adjustment_points AS gsPoints',
-    'paroms_points AS paromsPoints',
     'sdr_points AS sdrPoints',
     'sdr_conversion_points AS sdrConversionPoints',
     'available_points AS availablePoints',
@@ -31,10 +27,7 @@ module.exports = function (id, organisationLevel) {
         results.forEach(function (result) {
           result.capacity = percentageCalculator.calculatePercentage(result.totalPointsOverall, result.availablePoints).toFixed(1) + '%'
           result.cmsContribution = percentageCalculator.calculatePercentage(result.cmsPoints, result.availablePoints).toFixed(1) + '%'
-          result.gsContribution = percentageCalculator.calculatePercentage(result.gsPoints, result.availablePoints).toFixed(1) + '%'
           result.caseContribution = percentageCalculator.calculatePercentage(result.totalCasePoints, result.availablePoints).toFixed(1) + '%'
-          result.armsContribution = percentageCalculator.calculatePercentage(result.armsCommunityPoints + result.armsLicencePoints, result.availablePoints).toFixed(1) + '%'
-          result.paromsContribution = percentageCalculator.calculatePercentage(result.paromsPoints, result.availablePoints).toFixed(1) + '%'
           result.sdrContribution = percentageCalculator.calculatePercentage(result.sdrPoints, result.availablePoints).toFixed(1) + '%'
           result.fdrContribution = percentageCalculator.calculatePercentage(result.sdrConversionPoints, result.availablePoints).toFixed(1) + '%'
         })
