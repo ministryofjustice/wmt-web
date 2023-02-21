c57a2ad25444558a0485d1f755485581a33dc845
carlov20 <carlov20@users.noreply.github.com>
2023-02-21 15:46:21 +0000

    WFP-1840 amended permissions for exports

--- app/middleware/rolesMiddleware.js
+++ app/middleware/rolesMiddleware.js
@@ -4,7 +4,7 @@ module.exports = function () {
     if (res.locals.user) {
       res.locals.canViewAdmin = [userRoles.SUPER_USER, userRoles.APPLICATION_SUPPORT].includes(res.locals.user.user_role)
       res.locals.canExportReductions = [userRoles.SUPER_USER, userRoles.MANAGER].includes(res.locals.user.user_role)
-      res.locals.canExportOverview = [userRoles.SUPER_USER, userRoles.MANAGER, userRoles.STAFF].includes(res.locals.user.user_role)
+      res.locals.canExportOverview = [userRoles.SUPER_USER, userRoles.MANAGER].includes(res.locals.user.user_role)
     }
     next()
   }
