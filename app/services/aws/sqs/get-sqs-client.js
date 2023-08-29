7a1e610c029dc1715af6dbe3c411f29aa895032d
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-08-29 12:20:12 +0100

    remove all credentials from sqsclient

--- app/services/aws/sqs/get-sqs-client.js
+++ app/services/aws/sqs/get-sqs-client.js
@@ -1,8 +1,5 @@
 const { SQSClient } = require('@aws-sdk/client-sqs')
 
 module.exports = function (config) {
-  return new SQSClient({
-    region: config.region,
-    endpoint: config.endpoint
-  })
+  return new SQSClient()
 }
