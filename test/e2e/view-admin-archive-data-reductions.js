2b9273404be1f0ddf3c7fee28e385f79ef1947c6
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2023-11-30 15:17:36 +0000

    more fixes for e2e tests

--- test/e2e/view-admin-archive-data-reductions.js
+++ test/e2e/view-admin-archive-data-reductions.js
@@ -102,7 +102,7 @@ describe('Admin Archive Data Reductions Page', () => {
       const extraSearchCritera = await $('.select2-search__field')
       await extraSearchCritera.setValue('Test_Forename')
 
-      const criteriaName = await $('#select2-multi-search-field-results li[data-select2-id="15"]')
+      const criteriaName = await $('#select2-multi-search-field-results li[data-select2-id="16"]')
       await criteriaName.click()
 
       const search = await $('#archive-reductions-filter-submit')
