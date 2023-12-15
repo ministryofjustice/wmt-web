18da88e7e5d49bca0558cd3dbecb50adc3ea2f76
gregkhawkins <148795810+gregkhawkins@users.noreply.github.com>
2023-12-15 11:52:44 +0000

    WFP-2174: migrate auth endpoints to use manage-users-service (#1028)

--- app/data/manageUsersClient.js
+++ app/data/manageUsersClient.js
@@ -1,14 +1,14 @@
 const RestClient = require('./restClient')
 const config = require('../../config')
 
-const restClient = new RestClient(config.apis.hmppsAuth)
+const restClient = new RestClient(config.apis.manageUsersService)
 
 function getUserEmail (token) {
-  return restClient.get('/api/me/email', token)
+  return restClient.get('/users/me/email', token)
 }
 
 function getUser (token) {
-  return restClient.get('/api/user/me', token)
+  return restClient.get('/users/me', token)
 }
 
 module.exports = {
