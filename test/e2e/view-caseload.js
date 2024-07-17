e1983fdfd6c3f12e61277ce04e9aa6ef889096df
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-07-17 20:04:54 +0100

    Wfp 2564 spelling mistake (#1064)
    * WFP-2564 spelling correction
    
    * WFP-2564 fix test
    
    * WFP-2564 fix unit test data
    
    * WFP-2564 renaming constant
    
    * WFP-2564 changing caseload details
    
    * WFP-2564 changing caseload details - main caseload
    
    * WFP-2564 changing caseload details - main caseload - tests
    
    * WFP-2564 changing caseload details - main caseload - more tests
    
    * WFP-2564 changing caseload details - main caseload - more tests 2
    
    * WFP-2564 changing omic overview

--- test/e2e/view-caseload.js
+++ test/e2e/view-caseload.js
@@ -48,11 +48,11 @@ describe('View your caseload flow', () => {
       expect(Number(textTotal[0])).to.be.greaterThan(-1) // eslint-disable-line
       expect(textTotal[1]).to.eql('Community cases')
 
-      licence = await $('#licenseTotal')
+      licence = await $('#licenceTotal')
       licence = await licence.getText()
       textTotal = licence.split('\n')
       expect(Number(textTotal[0])).to.be.greaterThan(-1) // eslint-disable-line
-      expect(textTotal[1]).to.eql('License cases')
+      expect(textTotal[1]).to.eql('Licence cases')
     })
 
     it('with the correct tabs and tables', async () => {
@@ -72,10 +72,10 @@ describe('View your caseload flow', () => {
       await community.click()
       community = await $('.sln-table-caseload-community')
 
-      licence = await $('[href="#license"]')
+      licence = await $('[href="#licence"]')
       await licence.click()
       licence = await $('.sln-table-caseload-license')
-      const heading = await $('#license-enhanced .govuk-heading-m')
+      const heading = await $('#licence-enhanced .govuk-heading-m')
       const headingIsDisplayed = await heading.isDisplayed()
       return expect(headingIsDisplayed).to.be.true
     })
@@ -112,7 +112,7 @@ describe('View your caseload flow', () => {
       exists = await community.isExisting()
       expect(exists).to.be.equal(true)
 
-      licence = await $('#license-enhanced')
+      licence = await $('#licence-enhanced')
       exists = await licence.isExisting()
       expect(exists).to.be.equal(true)
 
@@ -124,10 +124,10 @@ describe('View your caseload flow', () => {
       await community.click()
       community = await $('.sln-table-caseload-community')
 
-      licence = await $('[href="#license"]')
+      licence = await $('[href="#licence"]')
       await licence.click()
       licence = await $('.sln-table-caseload-license')
-      const heading = await $('#license-enhanced .govuk-heading-m')
+      const heading = await $('#licence-enhanced .govuk-heading-m')
       const headingIsDisplayed = await heading.isDisplayed()
       return expect(headingIsDisplayed).to.be.true
     })
