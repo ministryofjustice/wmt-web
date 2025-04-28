2eff50166a01a48c2260e5a6eff1a2b02b7e2cad
katie.cawthorne <katie.cawthorne@digital.justice.gov.uk>
2025-04-28 11:17:40 +0100

    :white_check_mark: make the test wait for element to be displayed to try and fix bidi error

--- test/e2e/view-contracted-hours.js
+++ test/e2e/view-contracted-hours.js
@@ -80,6 +80,7 @@ describe('View contracted hours', function () {
     before(async function () {
       await authenticationHelp.login(authenticationHelp.users.SuperUser)
       await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+      await $('.govuk-breadcrumbs').waitForDisplayed()
     })
 
     it('should navigate to the workload owner contracted-hours page', async function () {
@@ -121,6 +122,7 @@ describe('View contracted hours', function () {
     before(async function () {
       await authenticationHelp.login(authenticationHelp.users.Staff)
       await browser.url(workloadOwnerDefaultUrl + '/contracted-hours')
+      await $('.govuk-heading-xl').waitForDisplayed()
     })
 
     it('should not be able to navigate to the workload owner contracted-hours page', async function () {
