6cf74a563cba0701bdb2f5ff7c56a842c9ef0fe6
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-06-26 11:29:51 +0100

    adding health endpoint

--- app/middleware/setUpHealthChecks.js
+++ app/middleware/setUpHealthChecks.js
@@ -3,6 +3,12 @@ const express = require('express')
 module.exports = function () {
   const router = express.Router()
 
+  router.get('/health', function (req, res) {
+    res.send({
+      status: 'ok'
+    })
+  })
+
   router.get('/ping', function (req, res) {
     res.send({
       status: 'UP'
