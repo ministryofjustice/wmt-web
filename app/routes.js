14ef0397d7361f61de034f27ed0cc73c096ded1b
iainjchambers-moj <iain.chambers@digital.justice.gov.uk>
2026-09-25 09:53:07 +0100

    Remove routes and add decomision page (#1212)
    * Remove routes and add decomision page
    
    * Fix typo
    
    * Actually fix typo
    
    ---------
    
    Co-authored-by: Iain Chambers <iainjchambers@gmail.com>

--- app/routes.js
+++ app/routes.js
@@ -3,24 +3,32 @@
  */
 
 const { Router } = require('express')
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const asyncMiddleware = require('./middleware/asyncMiddleware')
-const files = fs.readdirSync(path.resolve(__dirname, 'routes'))
+// const files = fs.readdirSync(path.resolve(__dirname, 'routes'))
 
 module.exports = function () {
   const router = Router()
-  const routes = files.map((file) => {
-    return require(path.resolve(__dirname, 'routes', file.replace('.js', '')))
-  })
+
+  // const routes = files.map((file) => {
+  //   return require(path.resolve(__dirname, 'routes', file.replace('.js', '')))
+  // })
+
   const get = function (path, handler) {
     router.get(path, asyncMiddleware(handler))
   }
-  const post = function (path, handler) {
-    router.post(path, asyncMiddleware(handler))
-  }
-  routes.forEach(function (route) {
-    route(get, post)
+
+  // const post = function (path, handler) {
+  //   router.post(path, asyncMiddleware(handler))
+  // }
+  // routes.forEach(function (route) {
+  //   route(get, post)
+  // })
+
+  get('*', function (_req, res) {
+    return res.render('decommission')
   })
+
   return router
 }
