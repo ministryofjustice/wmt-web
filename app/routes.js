188d23c1393de3957719e77fc9f5aa049fe0f693
Iain Chambers <iainjchambers@gmail.com>
2026-09-25 10:55:19 +0100

    Less aggressive routing override

--- app/routes.js
+++ app/routes.js
@@ -3,31 +3,28 @@
  */
 
 const { Router } = require('express')
-// const fs = require('fs')
-// const path = require('path')
+const fs = require('fs')
+const path = require('path')
 const asyncMiddleware = require('./middleware/asyncMiddleware')
-// const files = fs.readdirSync(path.resolve(__dirname, 'routes'))
+const files = fs.readdirSync(path.resolve(__dirname, 'routes'))
 
 module.exports = function () {
   const router = Router()
-
-  // const routes = files.map((file) => {
-  //   return require(path.resolve(__dirname, 'routes', file.replace('.js', '')))
-  // })
-
-  const get = function (path, handler) {
-    router.get(path, asyncMiddleware(handler))
+  const routes = files.map((file) => {
+    return require(path.resolve(__dirname, 'routes', file.replace('.js', '')))
+  })
+  const get = function (path, _handler) {
+    router.get(path, asyncMiddleware(function (_req, res) {
+      return res.render('decommission')
+    }))
   }
-
-  // const post = function (path, handler) {
-  //   router.post(path, asyncMiddleware(handler))
-  // }
-  // routes.forEach(function (route) {
-  //   route(get, post)
-  // })
-
-  get('*', function (_req, res) {
-    return res.render('decommission')
+  const post = function (path, _handler) {
+    router.post(path, asyncMiddleware(function (_req, res) {
+      return res.render('decommission')
+    }))
+  }
+  routes.forEach(function (route) {
+    route(get, post)
   })
 
   return router
